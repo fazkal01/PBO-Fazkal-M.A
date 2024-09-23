5b332746903b9c2a42084d0c91e6e1a88017f41a
fazkal01 <Fazkal2005@gmail.com>
2024-09-23 14:20:48 +0700

    Add files via upload

---
+++ Latihan6.2.php
@@ -0,0 +1,15 @@
+<?php
+class Perulangan {
+    public function loop() 
+    {
+        for ($i= 15;$i > 0; $i--)
+        {
+            echo $i;
+            echo "<br/>";
+        }
+    }
+}
+
+$objekPerulangan = new Perulangan ();
+echo "Jenis Perulangan Lainnya "."<br />";
+?>
